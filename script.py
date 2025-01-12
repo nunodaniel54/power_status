3bb90fa24906568d6521963a0f43906fd19b854a
nunodaniel54 <46898462+nunodaniel54@users.noreply.github.com>
2025-01-12 01:42:49 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import psycopg2
-import time
+import datetime
 from twilio.rest import Client
 
 def connect_to_db():
-    retry_attempts = 20
+    retry_attempts = 1
     for attempt in range(retry_attempts):
         # Connect to the database
         try:
@@ -14,55 +14,61 @@ def connect_to_db():
                 port="6543",
                 dbname="postgres"
             )
-            print("Connection successful!")
-            
-            # Create a cursor to execute SQL queries
             cursor = connection.cursor()
             
-            # Example query
-            #cursor.execute("select * from exemplo1")
-
-            cursor.execute("select id from exemplo1 ORDER BY id DESC LIMIT 1")
+            cursor.execute("select last_time_alive, notification from wake_up ORDER BY id DESC LIMIT 1")
+            
             result = cursor.fetchone()
-            print(result[0])
-            cursor.execute("Insert into exemplo1 (test) values (" + str(result[0]) + ");")
-            connection.commit()
-            cursor.execute("select * from exemplo1")
-            result = cursor.fetchall() 
-  
-            # Printing all records or rows from the table. 
-            # It returns a result set.  
-            for all in result: 
-                print(all)
-        # result = cursor.fetchone()
-        # print(result)
-
+          #  print(datetime.datetime.now())
+           # print(result[0])
+            
+            if(time_calculate(result[0],datetime.datetime.now()) > 12 and result[1]==0):
+                send_msg("Atenção!! Falha de Eletricidade.")
+                print("Correu mal")
+                cursor.execute("update wake_up set notification = 1")
+                connection.commit()
+            elif(time_calculate(result[0],datetime.datetime.now()) > 12 and result[1]==1):    
+                print("Continua mal")
+            else:
+                if(result[1]==1):
+                    send_msg("Eletricidade de volta!")
+                print("Correu tudo bem")
+                cursor.execute("delete from wake_up")
+                connection.commit()
+                
+                
             # Close the cursor and connection
             cursor.close()
             connection.close()
-            print("Connection closed.")
-            account_sid = 'AC98012587e10cecccf2c8eff9b86243c1'
-            auth_token = '408a38ccb7180467317ddca5ca41765f'
-            client = Client(account_sid, auth_token)
-
-            message = client.messages.create(
-              from_='whatsapp:+14155238886',
-             # content_sid='HXb5b62575e6e4ff6129ad7c8efe1f983e',
-              #content_variables='{"1":"12/1","2":"3pm"}',
-              to='whatsapp:+351961065823',
-              body = 'fodasse'
-            )
+           # print("Connection closed.")
+            
             return
         except Exception as e:
             print(f"Failed to connect: {e}")
-            time.sleep(1)
     return None
 
-connection = connect_to_db()
-if connection:
-    print("Connected to the database")
 
+def time_calculate(time1,time2):
 
+    td = time2 - time1
+   # print(td)
+    td_mins = int(round(td.total_seconds() / 60))
+    #print(td_mins)
 
+    return td_mins
 
+def send_msg(msg):
+    account_sid = 'AC98012587e10cecccf2c8eff9b86243c1'
+    auth_token = '408a38ccb7180467317ddca5ca41765f'
+    client = Client(account_sid, auth_token)
 
+    message = client.messages.create(
+    from_='whatsapp:+14155238886',
+    # content_sid='HXb5b62575e6e4ff6129ad7c8efe1f983e',
+    #content_variables='{"1":"12/1","2":"3pm"}',
+    to='whatsapp:+351961065823',
+    body = msg
+    )
+    
+    
+connect_to_db()
